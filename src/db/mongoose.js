235cdd7dfe6d99beb816135407761f5344a08b92
AbishekRajkumarS <abishek.rajkumar2000@gmail.com>
2020-06-21 15:13:16 +0530

    Mlab Configuredtest

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const URL = "mongodb://abishek:test123@ds031957.mlab.com:31957/heroku_0hjzw5cs" || "mongodb://127.0.0.1:27017/VITggy"
+const URL = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/VITggy"
 
 mongoose.connect(URL, {
     useNewUrlParser: true,
